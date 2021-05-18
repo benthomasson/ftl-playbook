1200aedb8c613c7fb77e892d39cf450b2b0cc8c7
Ben Thomasson <bthomass@redhat.com>
2021-05-18 06:12:06 -0400

    Turning off gather_facts during perf test

--- performance-test/ansible/local/run_perf.sh
+++ performance-test/ansible/local/run_perf.sh
@@ -4,9 +4,9 @@ ulimit -Sn 10000
 ../../scripts/perf_test.py x x x x x --only-header
 for t in argtest
 do
-for m in 1 10
+for m in 1 10 100
 do
-for n in 1 10
+for n in 1 10 100
 do
 ../../scripts/perf_test.py "ansible-playbook -M modules -i inventory${m}.yml playbook${n}.yml" ansible-playbook ${t} ${n} ${m} --no-header
 done
