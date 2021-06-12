502cc59ff308c8453736e8de759b24021aed8990
Ben Thomasson <bthomass@redhat.com>
2021-06-12 05:55:37 -0400

    Requires python 3.9

--- python-asyncio/ftl_playbook/ftl_playbook/playbook.py
+++ python-asyncio/ftl_playbook/ftl_playbook/playbook.py
@@ -1,5 +1,6 @@
 from faster_than_light import run_module
 import yaml
+import os
 
 
 def load_playbook(playbook_path):
@@ -15,12 +16,18 @@ def get_module_name(task):
 
 
 async def playbook_interpreter(playbook, inventory, module_dirs):
+    term_width = os.get_terminal_size()[0]
     gate_cache = {}
     for play in playbook:
-        print(play)
+        #print(play)
         tasks = play.get('tasks', [])
         hosts = play.get('hosts', [])
         name = play.get('name', '')
+        print()
+        print(f'PLAY [{name}] '.ljust(term_width, '*'))
         for task in tasks:
+            print ()
+            print (f'TASK [{get_module_name(task)}] '.ljust(term_width, '*'))
             output = await run_module(inventory, module_dirs, get_module_name(task), gate_cache=gate_cache, modules=[get_module_name(task)])
-            print(output)
+            #for i in output:
+            #    print("ok:", f'[{i}]')
