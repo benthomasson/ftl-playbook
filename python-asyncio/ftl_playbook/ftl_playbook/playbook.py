8c863cdd12d461b4069b72f684150d21602e5a83
Ben Thomasson <ben.thomasson@gmail.com>
2022-11-15 22:15:48 -0500

    Adds play recap

--- python-asyncio/ftl_playbook/ftl_playbook/playbook.py
+++ python-asyncio/ftl_playbook/ftl_playbook/playbook.py
@@ -1,6 +1,8 @@
 from faster_than_light import run_module, run_ftl_module
 import yaml
 import os
+from pprint import pformat
+from collections import defaultdict
 
 
 def load_playbook(playbook_path):
@@ -15,19 +17,48 @@ def get_module_name(task):
             return key
 
 
+def get_module_args(task, module_name):
+    return task[module_name]
+
+
+def get_hosts_from_pattern(inventory, hosts):
+    return hosts.split(",")
+
+
 async def playbook_interpreter(playbook, inventory, module_dirs):
-    term_width = os.get_terminal_size()[0]
+    try:
+        term_width = os.get_terminal_size()[0]
+    except Exception:
+        term_width = 80
     gate_cache = {}
+    results = defaultdict(lambda :dict(ok=0, failed=0, changed=0, unreachable=0))
     for play in playbook:
-        #print(play)
-        tasks = play.get('tasks', [])
-        hosts = play.get('hosts', [])
-        name = play.get('name', '')
+        print(play)
+        tasks = play.get("tasks", [])
+        hosts = get_hosts_from_pattern(inventory, play.get("hosts", ""))
+        name = play.get("name", "")
         print()
-        print(f'PLAY [{name}] '.ljust(term_width, '*'))
+        print(f"PLAY [{name}] ".ljust(term_width, "*"))
         for task in tasks:
-            print ()
-            print (f'TASK [{get_module_name(task)}] '.ljust(term_width, '*'))
-            output = await run_ftl_module(inventory, module_dirs, get_module_name(task), gate_cache=gate_cache, modules=[get_module_name(task)])
-            #for i in output:
-            #    print("ok:", f'[{i}]')
+            module_name = get_module_name(task)
+            print()
+            print(f"TASK [{module_name}] ".ljust(term_width, "*"))
+            output = await run_ftl_module(
+                inventory,
+                module_dirs,
+                module_name,
+                gate_cache=gate_cache,
+                modules=[get_module_name(task)],
+                module_args=get_module_args(task, module_name),
+            )
+            for host, result in output.items():
+                print(f"ok: [{host}] => {pformat(result)}")
+                results[host]['ok'] += 1
+
+    print(f"\nPLAY RECAP".ljust(term_width, "*"))
+    for host in hosts:
+        print(f"{host}".ljust(26),
+              f": ok={results[host]['ok']}   ",
+              f"changed={results[host]['changed']}   ",
+              f"failed={results[host]['failed']}   ",
+              f"unreachable={results[host]['unreachable']}")
